01b54242d78871894b44e680dd755eb99bfb37bb
kubat <kubat.aiupov@gmail.com>
2023-09-12 21:37:49 +0200

    add POST request

--- src/App.js
+++ src/App.js
@@ -9,27 +9,42 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = useCallback(async function() {
+  const swapiUrl = "https://swapi.dev/api/films/";
+
+  const fetchMoviesHandler = useCallback(async function () {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-584f4-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
       const data = await response.json();
 
-      const transformedData = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedData);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      // const transformedData = data.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (error) {
-      setError(error.message)
+      setError(error.message);
     }
     setIsLoading(false);
     // setIsLoading(true);
@@ -50,20 +65,32 @@ function App() {
     //     setMovies(transformedData);
     //     setIsLoading(false);
     //   }).catch(error => setError(error));
-  },[]);
+  }, []);
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchMoviesHandler();
-  },[fetchMoviesHandler]);
+  }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie)
+  async function addMovieHandler(movie) {
+    //console.log(movie)
+    const response = await fetch(
+      "https://react-http-584f4-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
 
   return (
     <React.Fragment>
       <section>
-        <AddMovie onAddMovie= {addMovieHandler}/>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
